0f2be62088da89b388640c30e67838a010e76803
CLARA ELIS DA SILVA LARSEN <08637875173@ifms.edu.br>
2024-03-19 10:14:35 -0400

    Box estudante na main.tsx

--- src/componentes/Main.tsx
+++ src/componentes/Main.tsx
@@ -1,9 +1,17 @@
+import './Main.css'
 function Main(){
     return(
-        <> 
-          <main>Main</main>
-          <h1>Meu h1</h1>
-        </>
+            <main>
+              <div className='estudante-box'>
+                 <img className='estudante-foto' src="https://github.com/Claraeex.png" alt=""/> 
+                 <div className='informacoes'>
+                  <h3 className= "estudante_nome">ClaraElis </h3>
+                 <p className= "estudante_descrição">Comedora de Novinhos rs</p>
+                 <a className= 'link-git' href="https://github.com/Claraeex">GithHub</a>
+                 </div>
+              </div>
+          </main>
+       
     )
 }
 export default Main;
